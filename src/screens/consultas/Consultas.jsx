09911506f14e8b381fe41f883566d653a3ca9fc6
Lilith0806 <AQA04>
2024-10-18 18:56:36 -0500

    se sube el front

--- src/screens/consultas/Consultas.jsx
+++ src/screens/consultas/Consultas.jsx
@@ -2,34 +2,62 @@ import React from 'react';
 import Menu from '../../menu/Menu';
 import Footer from '../../footer/Footer';
 import lienzo from '/src/components/lienzoEjemplo.jpg';
+import './consultas.css'
 
 const Consultas = () => {
+  const handleButtonClick = () => {
+    window.location.href = './carrito';
+  };
+
+  
   return (
-    <>
-      <Menu />
-      <div id='padresote'>
-        <div id="cartas">
-          <img src={lienzo} className='imagenesCartas' />
-          <div>Lienzo</div>
-          <div>Tamaño 30x30</div>
-          <button className="botonCartas">Agregar al Carrito</button>
+  <>
+    <Menu/>
+    <div>
+        <div id="tituloConsultas"> Lienzos </div>
+
+        <div id="expoConsultas">
+            <div id='cartasConsultas'>
+              <img src={lienzo} alt='estucheEjemplo' class='imagenesConsultas'></img>
+              <div class='descripcionConsultas'>
+                <p>Lienzo</p>
+                <p>20x30</p>
+              </div>
+              <button class='botonConsultas' onClick={handleButtonClick}>Comprar</button>
+            </div>
+
+            <div id='cartasConsultas'>
+              <img src={lienzo} alt='lienzoEjemplo' class='imagenesConsultas'></img>
+              <div class='descripcionConsultas'>
+                <p>Lienzos</p>
+                <p>30x45</p>
+              </div>
+              <button class='botonConsultas' onClick={handleButtonClick}>Comprar</button>
+            </div>
+
+            <div id='cartasConsultas'>
+              <img src={lienzo} alt='acrilicoEjemlpo' class='imagenesConsultas'></img>
+              <div class='descripcionConsultas'>
+                <p>Lienzo</p>
+                <p>20x20</p>
+              </div>
+              <button class='botonConsultas'onClick={handleButtonClick}>Comprar</button>
+              </div>
+
+            <div id='cartasConsultas'>
+              <img src={lienzo} alt='digitalEjemplo' class='imagenesConsultas'></img>
+              <div class='descripcionConsultas'>
+                <p>Lienzo</p>
+                <p>20x40</p>
+              </div>
+              <button class='botonConsultas'onClick={handleButtonClick}>Comprar</button>
+
+            </div>
+
         </div>
 
-        <div id="cartas">
-          <img src={lienzo} className='imagenesCartas' />
-          <div>Lienzo</div>
-          <div>Tamaño 3x3</div>
-          <button className="botonCartas">Agregar al Carrito</button>
-        </div>
-
-        <div id="cartas">
-          <img src={lienzo} className='imagenesCartas' />
-          <div>Lienzo</div>
-          <div>Tamaño 3x3</div>
-          <button className="botonCartas">Agregar al Carrito</button>
-        </div>
-      </div>
-      <Footer />
+    </div>
+    <Footer/>
     </>
   );
 }
