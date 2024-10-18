373f584a29607912f44ffd4ebf021d895df0d2b0
Lilith0806 <AQA04>
2024-10-17 22:51:10 -0500

    Se termina la galeria

--- src/screens/Home/galeria/galeria.jsx
+++ src/screens/Home/galeria/galeria.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import './galeria.css'
 
+//imagenes
+import img1 from '/src/Components/galeriaKillua.jpg'
+import img2 from '/src/Components/galeriaEdward.jpg'
+import img3 from '/src/Components/galeriaGanyu.jpg'
+import img4 from '/src/Components/galeriaWinry.jpg'
+import img5 from '/src/Components/galeriaDavier.jpg'
+
+
 const galeria = () => {
   return (
     <>
@@ -8,11 +16,11 @@ const galeria = () => {
         <div className='tituloGaleria'>Mi Coleccion</div>
         
         <div class="galeria">
-            <div className='imgGaleria'>imagen1</div>
-            <div className='imgGaleria'>imagen1</div>
-            <div className='imgGaleria'>imagen1</div>
-            <div className='imgGaleria'>imagen1</div>
-            <div className='imgGaleria'>imagen1</div>
+            <img src={img1} className='imgGaleria img-grande'></img>
+            <img src={img2} className='imgGaleria img-mediana'></img>
+            <img src={img3} className='imgGaleria img-peque'></img>
+            <img src={img4} className='imgGaleria img-mediana'></img>
+            <img src={img5} className='imgGaleria img-grande'></img>
         </div>
 
     </div>
