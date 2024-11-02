944bb995ac8a81c02daa958b38308008530b773c
Lilith0806 <AQA04>
2024-11-02 03:10:15 -0500

    empieza el backend con login

---
+++ BACK/database.js
@@ -0,0 +1,12 @@
+import mysql from 'mysql2/promise';
+
+const createConnection = async () => {
+  return await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'alejandro717',
+    database: 'cherryBlossom'
+  });
+};
+
+export default createConnection;
