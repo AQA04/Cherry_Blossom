0d8110c4c058481f8ef5b231442a095e9848f241
Lilith0806 <AQA04>
2024-11-09 14:37:56 -0500

    se crea la ruta historial y sus documentos

---
+++ FRONT/src/screens/historial/Historial.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Historial.css'
+
+import Menu from '../../menu/Menu'
+import Footer from '../../footer/Footer'
+
+const Historial = () => {
+  return (
+    <>
+    <Menu/>
+
+    <div>
+        Bienvenido al historial cochinota
+    </div>
+
+    <Footer/>
+    </>
+  )
+}
+
+export default Historial
