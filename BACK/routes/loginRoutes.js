fdad9d9c21f725e47a13939ba2ba1c0807db985d
Lilith0806 <AQA04>
2024-11-11 17:02:49 -0500

    finalizacion backend login

--- BACK/routes/loginRoutes.js
+++ BACK/routes/loginRoutes.js
@@ -5,22 +5,13 @@ import LoginControl from '../controllers/LoginControl.js'; // Importar la funci�
 const router = express.Router();
 
 // Definir la ruta POST para autenticar a un usuario
-router.post('/', async (req, res) => {
-    const { Correo, userPsw_ctl } = req.body; // Obtener el correo y la contraseña del cuerpo de la solicitud
+router.get('/', async (req, res) => {
+    const results = await LoginControl();
+    
     try {
-        const results = await LoginControl(Correo, userPsw_ctl);
-        
-        // Verificar si se encontró un usuario
-        if (results.length === 0) {
-            return res.status(401).json({ success: false, message: 'Usuario no encontrado o contraseña incorrecta' });
-        }
-
-        // Si la autenticación es exitosa, devolver un objeto con éxito y los datos del usuario
-        return res.json({ success: true, user: results[0] }); // Retornar el primer usuario encontrado
-
+        res.json(results); // Enviar los resultados como respuesta
     } catch (error) {
-        console.error('Error al buscar usuario:', error);
-        res.status(500).json({ success: false, message: 'Error al buscar usuario' });
+        res.status(500).send('Error al buscar usuario');
     }
 });
 
